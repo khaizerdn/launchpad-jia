773dddeeab46528831a300afce93cedd34334c54
khaizerdn <khaizerdn@gmail.com>
2025-11-12 11:49:25 +0800

    Update SuperAdminUtils.tsx

--- src/lib/SuperAdminUtils.tsx
+++ src/lib/SuperAdminUtils.tsx
@@ -2,10 +2,13 @@ export const superAdminList = [
   "bryce.mercines@whitecloak.com",
   "vince.carandang@whitecloak.com",
   "rafael.tiongson@whitecloak.com",
+  "rafa.tiongson@whitecloak.com",
   "donn.gamboa@whitecloak.com",
   "anne.liango@whitecloak.com",
   "miguel.fermin@whitecloak.com",
   "michelle.cruz@whitecloak.com",
   "anne.liangco@whitecloak.com",
+  "thea.juego@whitecloak.com",
+  "cc.khaizer.noguera@cvsu.edu.ph",
   "khaizerdn@gmail.com",
 ];
